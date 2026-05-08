1a076e4039507e5c3a8658c94382b3d631c7b83e
Sara Romao <sara-romao@live.com>
2026-05-07 23:11:20 -0300

    adicionado lista de projetos

---
+++ script/projects-data.js
@@ -0,0 +1,248 @@
+const projects =[
+  {
+    "id": "feedback",
+    "title": "Caixa de Feedbacks",
+    "description": "Sistema completo de feedback permitir recebam sugestões, críticas e comentários de forma anônima, por meio de um código único compartilhado com os usuários.",
+
+    "links": {
+      "code": {
+        "url": "https://github.com/Sara01romao/feedback-nodejs-typescript-docker-postgres-nextjs",
+        "icon": "fa-solid fa-code"
+      },
+
+      "access": {
+        "url": "",
+        "icon": ""
+      },
+    },
+
+    "image": {
+      "src": "./imgs/feedback.png",
+      "alt": "Sistema de feedback"
+    },
+
+    "miniGuide": {
+      "tech": [
+        "NextJs",
+        "ReactJs",
+        "TypeScript",
+        "NodeJs",
+        "ExpressJs",
+        "PostgreSQL",
+        "Docker",
+        "Prisma",
+        "JWT",
+        "Figma"
+      ],
+
+      "typography": [
+        {
+          "font": "Inter",
+          "weights": [
+            "Bold",
+            "Medium",
+            "Normal"
+          ]
+        }
+      ],
+
+      "colors": [
+        "#0F172A",
+        "#6366F1",
+        "#FFFFFF"
+      ]
+    }
+  },
+
+  {
+    "id": "bot-telegram-cripto",
+    "title": "Bot Telegram Cripto",
+    "description": "Bot para Telegram desenvolvido para consultar e enviar informações do mercado de criptomoedas em tempo real utilizando APIs externas, permitindo automação de consultas e monitoramento de preços diretamente no chat.",
+
+    "links": {
+      "code": {
+        "url": "https://github.com/Sara01romao/bot-telegram-cripto-nodejs-fastify-api",
+        "icon": "fa-solid fa-code"
+      },
+
+      "access": {
+        "url": "",
+        "icon": ""
+      }
+    },
+
+    "image": {
+      "src": "./imgs/bot.png",
+      "alt": "Bot Telegram de criptomoedas"
+    },
+
+    "miniGuide": {
+      "tech": [
+        "NodeJs",
+        "JavaScript",
+        "Fastify",
+        "Telegram Bot API",
+        "REST API"
+      ],
+
+      "typography": [],
+
+      "colors": []
+    }
+  },
+  
+  {
+    "id": "cinema",
+    "title": "Ingresso Cinema",
+    "description": "Este projeto permite que os usuários escolham filmes, datas, horários e tipos de ingressos. Utiliza estados reativos para gerenciar a disponibilidade de poltronas (livres, ocupadas ou reservadas).",
+
+    "links": {
+      "code": {
+        "url": "https://github.com/Sara01romao/cinema-ingresso-reactjs-typescript",
+        "icon": "fa-solid fa-code"
+      },
+
+      "access": {
+        "url": "https://sistema-ingresso-cinema.netlify.app",
+        "icon": "fa-solid fa-arrow-up-right-from-square"
+      },
+      
+    },
+
+    "image": {
+      "src": "./imgs/project-movies.png",
+      "alt": "Ingresso Cinema"
+    },
+
+    "miniGuide": {
+      "tech": [
+        "ReactJs",
+        "TypeScript",
+        "Figma"
+      ],
+
+      "typography": [
+        {
+          "font": "Helvetica",
+          "weights": [
+            "Bold",
+            "Medium",
+            "Normal"
+          ]
+        }, 
+      ],
+
+      "colors": [
+        "#0f0418",
+        "#ffffff"
+      ]
+    }
+  },
+
+  {
+    "id": "ia-frase",
+    "title": "WebFrase IA",
+    "description": "Aplicação web para gerar frases automáticas para posts em datas comerciais e comemorativas com Inteligência Artificial, facilitando a criação de conteúdo para redes sociais.",
+
+    "links": {
+      "code": {
+        "url": "https://github.com/Sara01romao/webfrase-react-typescript-ia-nodejs-express?tab=readme-ov-file",
+        "icon": "fa-solid fa-code"
+      },
+
+      "access": {
+        "url": "",
+        "icon": ""
+      },
+      
+    },
+
+    "image": {
+      "src": "./imgs/project-ia.png",
+      "alt": "Gerador de frase com ia"
+    },
+
+    "miniGuide": {
+      "tech": [
+        "ReactJs",
+        "TypeScript",
+        "NodeJs",
+        "TypeScript",
+        "ExpressJs",
+        "Figma"
+      ],
+
+      "typography": [
+        {
+          "font": "Inter",
+          "weights": [
+            "Bold",
+            "Medium",
+            "Normal"
+          ]
+        }, 
+      ],
+
+      "colors": [
+        "#FF6C47",
+        "#ffffff"
+      ]
+    }
+  },
+  {
+    "id": "chat",
+    "title": "Chat",
+    "description": "Chat em tempo real desenvolvido com Node.js, Express, jQuery e Socket.IO, permitindo troca instantânea de mensagens entre usuários.",
+
+    "links": {
+      "code": {
+        "url": "https://github.com/Sara01romao/chat-socket-io",
+        "icon": "fa-solid fa-code"
+      },
+
+      "access": {
+        "url": "https://www.youtube.com/shorts/txRU-mOOFHo",
+        "icon": "fa-brands fa-youtube"
+      }
+    },
+
+    "image": {
+      "src": "./imgs/project-chat.png",
+      "alt": "Chat em tempo real"
+    },
+
+    "miniGuide": {
+      "tech": [
+        "HTML",
+        "CSS",
+        "jQuery",
+        "NodeJs",
+        "Express",
+        "Socket.io",
+        "Figma"
+      ],
+
+      "typography": [
+        {
+          "font": "Inter",
+          "weights": [
+            "Bold",
+            "Medium",
+            "Normal"
+          ]
+        },
+        {
+          "font": "Inria Sans",
+          "weights": [
+            "Bold"
+          ]
+        }
+      ],
+
+      "colors": [
+        "#34C081",
+        "#1E1E1E"
+      ]
+    }
+  }
+]
