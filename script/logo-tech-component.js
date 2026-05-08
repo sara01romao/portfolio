2b294763a99a7920ba457990f797dd1baa1d9793
Sara Romao <sara-romao@live.com>
2026-05-07 23:12:29 -0300

    crido componente da logo das tech

---
+++ script/logo-tech-component.js
@@ -0,0 +1,88 @@
+const techLogos = [
+  // Front-end
+  {
+    name: "React.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+  },
+  {
+    name: "Next.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+  },
+  {
+    name: "JavaScript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+  },
+  {
+    name: "TypeScript",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+  },
+  {
+    name: "jQuery",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg"
+  },
+  {
+    name: "HTML5",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+  },
+  {
+    name: "CSS3",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+  },
+  {
+    name: "Tailwind CSS",
+    img: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+  },
+  {
+    name: "Bootstrap",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+  },
+
+  // Back-end & Banco de Dados
+  {
+    name: "Node.js",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+  },
+  {
+    name: "REST API",
+    img: "https://cdn-icons-png.flaticon.com/512/2165/2165004.png"
+  },
+  {
+    name: "PostgreSQL",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
+  },
+  {
+    name: "MySQL",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+  },
+  {
+    name: "JSON",
+    img: "https://cdn-icons-png.flaticon.com/512/136/136525.png"
+  },
+  {
+    name: "Integração de Sistemas",
+    img: "https://cdn-icons-png.flaticon.com/512/1995/1995485.png"
+  },
+
+  // Ferramentas & Design
+  {
+    name: "Git",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+  },
+  {
+    name: "Docker",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+  },
+  {
+    name: "Figma",
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
+  },
+  {
+    name: "Postman",
+    img: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+  }
+]
+
+techLogos.forEach(logo =>{
+  let image = $("<img>", {src: logo.img, alt:logo.name});
+  $(".logos-tech").append(image);
+})
